766df7d9bd18312be4d731fc50fc727a90394c47
xyhxx <603331487@qq.com>
2022-09-25 12:41:42 +0800

    add models

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ import {StrictMode} from 'react';
 
 const queryClient = new QueryClient();
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vitest/config';
 import {resolve} from 'path';
 import react from '@vitejs/plugin-react';
 import {visualizer} from 'rollup-plugin-visualizer';
+import {PluginOption} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
         babelrc: true,
       },
     }),
-    visualizer(),
+    visualizer() as PluginOption,
   ],
   css: {
     modules: {
@@ -27,13 +28,14 @@ export default defineConfig({
     alias: {
       '@styles': resolve(__dirname, 'src/styles'),
       '@assets': resolve(__dirname, 'src/assets'),
-      '@routes': resolve(__dirname, 'src/routes/index'),
-      '@apis': resolve(__dirname, 'src/apis/index'),
-      '@components': resolve(__dirname, 'src/components/index'),
-      '@hooks': resolve(__dirname, 'src/hooks/index'),
+      '@routes': resolve(__dirname, 'src/routes'),
+      '@apis': resolve(__dirname, 'src/apis'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@hooks': resolve(__dirname, 'src/hooks'),
       '@pages': resolve(__dirname, 'src/pages'),
-      '@stores': resolve(__dirname, 'src/stores/index'),
-      '@utils': resolve(__dirname, 'src/utils/index'),
+      '@stores': resolve(__dirname, 'src/stores'),
+      '@models': resolve(__dirname, 'src/models'),
+      '@utils': resolve(__dirname, 'src/utils'),
     },
   },
 });
