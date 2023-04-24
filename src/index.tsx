f15d14a2fe0d14a516e5a19c1ba66539baeee4b4
xyh <603331487@qq.com>
2023-04-24 09:11:21 +0800

    refactor: extract the queryClient to utils

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,16 @@ import '@styles/index.css';
 import routes from '@routes';
 import {createRoot} from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {QueryClientProvider} from '@tanstack/react-query';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {StrictMode} from 'react';
-
-const queryClient = new QueryClient();
+import {QUERY_CLIENT} from '@utils';
 
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={QUERY_CLIENT}>
       <RouterProvider router={routes} />
       <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
     </QueryClientProvider>
