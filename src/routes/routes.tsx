d8adf88527ca9710fd26377912dda2eb783fc0c2
xyhxx <603331487@qq.com>
2023-05-08 21:33:08 +0800

    style: format

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,10 +3,8 @@ import {Home} from '@pages';
 import {HOME_PATH} from './name';
 import {RouteObject, useRoutes} from 'react-router-dom';
 
-const routes: RouteObject[] = [
-  {path: HOME_PATH, element: <Home />},
-];
-const PrivateRoutes: FC = function() {
+const routes: RouteObject[] = [{path: HOME_PATH, element: <Home />}];
+const PrivateRoutes: FC = function () {
   const Routes = useRoutes(routes);
 
   return Routes;
