e02e57dd4c7c7d55de67e3e0829abd41b943ab25
xyhxx <603331487@qq.com>
2023-05-13 21:32:17 +0800

    chore: modify cypress getById comment

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,7 +1,8 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     /**
-     * Custom command to select DOM element by data-testid attribute.
+     * Custom command to select DOM element by id attribute.
+     * @param {string} value - id
      * @example cy.getById('title')
      */
     getById(value: string): Chainable<JQuery<HTMLElement>>;
